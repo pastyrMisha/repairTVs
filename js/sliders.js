363db847a875438c550f0a2821af3f445e1d476f
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-02 15:34:45 +0300

    process7

--- js/sliders.js
+++ js/sliders.js
@@ -1,7 +1,18 @@
 let offset = 0;
 const sliderLine = document.querySelector('.section-brands-content__slider-line');
 
-document.querySelector('.section-brands-content__button-prev').addEventListener('click', function () {
+document.querySelector('.section-brands-content__button-next').addEventListener('click', function () {
     offset += 236;
-    sliderLine.style.left = offset + 'px';
+    if (offset > 2596) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.section-brands-content__button-prev').addEventListener('click', function () {
+    offset -= 236;
+    if (offset < 0) {
+        offset = 1180;
+    }
+    sliderLine.style.left = -offset + 'px';
 });
