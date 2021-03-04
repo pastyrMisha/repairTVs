1b619633927d8133405f2a0ec8297285bf1e716d
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-04 03:33:01 +0300

    ref11

--- footer_sendmail.php
+++ footer_sendmail.php
@@ -13,7 +13,7 @@
     // От кого письмо
     $mail->setFrom('support@skuptv.ru', 'Скупка TV');
     // Кому отправить
-    $mail->addAddress('skupka-tv@mail.ru');
+    // $mail->addAddress('skupka-tv@mail.ru');
     $mail->addAddress('skkonkurent@yandex.ru');
     // Тема письма
     $mail->Subject = 'Заявка на узнать цену ТВ с сайта Скупка TV';
