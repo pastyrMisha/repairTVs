919b6ae4947c46e2fc1ca5481fbff3ed4bf87422
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-06 21:31:04 +0300

    slider_swape

--- js/sliders.js
+++ js/sliders.js
@@ -67,6 +67,56 @@ document.querySelector('.section-brands-content__button-prev').addEventListener(
     sliderLine.style.left = -offset + 'px';
 });
 
+// swipe
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart(event) {
+    const firstTouch = event.touches[0];
+
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+    console.log(x1, y1);
+}
+
+function handleTouchMove(event) {
+    if(!x1 || !y1) {
+        return false;
+    }
+    let x2 = event.touches[0].clientX;
+    let y2 = event.touches[0].clientY;
+    // console.log(x2, y2);
+    let xDiff = x2 - x1;
+    let yDiff = y2 - y1;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        // right - left
+        if (xDiff > 0) sliderLine.textContent = 'right';
+        else sliderLine.textContent = 'left';
+    }
+    else {
+        // top - bottom
+        if (yDiff > 0) sliderLine.textContent = 'down';
+        else sliderLine.textContent = 'top';
+    }
+    x1 = null;
+    y1 = null;
+}
+
+// /swipe
+
+
+
+
+
+
+
+
+
 // === / brands ===
 
 // === footercall ===
