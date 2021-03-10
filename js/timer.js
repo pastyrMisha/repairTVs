d339378d8f239ac3fc6a11c672d00ded9261a0ff
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-10 14:07:06 +0300

    reff

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,13 @@
-let deadLine = '2021-03-13';
+let deadLine = '2021-03-14';
 
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
     seconds = Math.floor((t/1000) % 60),
     minutes = Math.floor((t/1000/60) % 60),
-    minutes = Math.floor((t/1000/60) % 60),
-    hours = Math.floor((t/1000/60/60) % 24),
-    days = Math.floor((t/(1000*60*60*24)));
+    hours = Math.floor((t/1000/60/60) % 24);
 
     return {
         'total' : t,
-        'days' : days,
         'hours' : hours,
         'minutes' : minutes,
         'seconds' : seconds
@@ -19,15 +16,13 @@ function getTimeRemaining(endtime) {
 
 function setClock(id, endtime) {
     let timer = document.getElementById(id),
-        days = timer.querySelector('.days'),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds');
+        hours = timer.querySelector('.section-header-wrapper__form-sale__timer-hours'),
+        minutes = timer.querySelector('.section-header-wrapper__form-sale__timer-minutes'),
+        seconds = timer.querySelector('.section-header-wrapper__form-sale__timer-seconds');
         timeInterval = setInterval(updateClock, 1000);
 
     function updateClock() {
         let t = getTimeRemaining(endtime);
-        days.textContent = t.days;
         hours.textContent = t.hours;
         minutes.textContent = t.minutes;
         seconds.textContent = t.seconds;
