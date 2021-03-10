a532ed70158139b98df49a2c280c160beb71775b
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-10 13:12:30 +0300

    timer_complette

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-let deadLine = '2021-10-21';
+let deadLine = '2021-03-13';
 
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
@@ -9,7 +9,33 @@ function getTimeRemaining(endtime) {
     days = Math.floor((t/(1000*60*60*24)));
 
     return {
-
+        'total' : t,
+        'days' : days,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds
     };
-
 }
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+        days.textContent = t.days;
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+        
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('timer', deadLine);
