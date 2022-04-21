7e22cdcf0ae4ccaecc87b56af04c8e89b846626d
Mikhail Zvendinov <skturnercz@gmail.com>
2022-04-21 15:56:01 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const postCss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
